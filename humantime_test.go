83ba717306832bd83e8b7786fc8b9a3ab5269831
Kevin Mulvey <kmulvey@linux.com>
2022-07-25 15:27:34 -0600

    s/now/today/g

--- humantime_test.go
+++ humantime_test.go
@@ -7,60 +7,60 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var now = time.Now()
+var today = time.Now()
 
 func init() {
-	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	today = time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, today.Location())
 }
 
 var TestParseTimeStringTestCases = map[string]time.Time{
-	"  at  3pm":  time.Date(now.Year(), now.Month(), now.Day(), 15, 0, 0, 0, now.Location()),
-	"at  12pm":   time.Date(now.Year(), now.Month(), now.Day(), 12, 0, 0, 0, now.Location()),
-	"  at  12am": time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()),
+	"  at  3pm":  time.Date(today.Year(), today.Month(), today.Day(), 15, 0, 0, 0, today.Location()),
+	"at  12pm":   time.Date(today.Year(), today.Month(), today.Day(), 12, 0, 0, 0, today.Location()),
+	"  at  12am": time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, today.Location()),
 }
 
 func TestParseTimeString(t *testing.T) {
 	t.Parallel()
 
-	var st, err = NewString2Time(now.Location())
+	var st, err = NewString2Time(today.Location())
 	assert.NoError(t, err)
 
-	result, err := st.parseTimeString(now, "  at  3pm")
+	result, err := st.parseTimeString(today, "  at  3pm")
 	assert.NoError(t, err)
-	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day(), 15, 0, 0, 0, now.Location()), result)
+	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day(), 15, 0, 0, 0, today.Location()), result)
 
-	result, err = st.parseTimeString(now, "at 12pm")
+	result, err = st.parseTimeString(today, "at 12pm")
 	assert.NoError(t, err)
-	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day(), 12, 0, 0, 0, now.Location()), result)
+	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day(), 12, 0, 0, 0, today.Location()), result)
 
-	result, err = st.parseTimeString(now, "at 12am")
+	result, err = st.parseTimeString(today, "at 12am")
 	assert.NoError(t, err)
-	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()), result)
+	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, today.Location()), result)
 }
 
 func TestParseDatePhrase(t *testing.T) {
 	t.Parallel()
 
-	var st, err = NewString2Time(now.Location())
+	var st, err = NewString2Time(today.Location())
 	assert.NoError(t, err)
 
 	result, err := st.parseDatePhrase("  1pm")
 	assert.NoError(t, err)
-	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day(), 13, 0, 0, 0, now.Location()), result)
+	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day(), 13, 0, 0, 0, today.Location()), result)
 
 	result, err = st.parseDatePhrase("yesterday  at  3pm")
 	assert.NoError(t, err)
-	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day()-1, 15, 0, 0, 0, now.Location()), result)
+	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day()-1, 15, 0, 0, 0, today.Location()), result)
 
 	result, err = st.parseDatePhrase("last tuesday   at   3pm")
 	assert.NoError(t, err)
-	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day()-int(now.Weekday()-time.Tuesday)-7, 15, 0, 0, 0, now.Location()), result)
+	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day()-int(today.Weekday()-time.Tuesday)-7, 15, 0, 0, 0, today.Location()), result)
 
 	result, err = st.parseDatePhrase("next sunday   at   12pm")
 	assert.NoError(t, err)
-	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day()-int(now.Weekday()-time.Sunday)+7, 12, 0, 0, 0, now.Location()), result)
+	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day()-int(today.Weekday()-time.Sunday)+7, 12, 0, 0, 0, today.Location()), result)
 
 	result, err = st.parseDatePhrase("this sunday   at   12am")
 	assert.NoError(t, err)
-	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day()-int(now.Weekday()-time.Sunday), 0, 0, 0, 0, now.Location()), result)
+	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day()-int(today.Weekday()-time.Sunday), 0, 0, 0, 0, today.Location()), result)
 }
