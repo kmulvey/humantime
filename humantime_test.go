73bb6b105194389d951f73920aa92a208bfb5764
Kevin Mulvey <kmulvey@linux.com>
2022-07-25 15:44:41 -0600

    test case maps

--- humantime_test.go
+++ humantime_test.go
@@ -19,23 +19,25 @@ var TestParseTimeStringTestCases = map[string]time.Time{
 	"  at  12am": time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, today.Location()),
 }
 
+var TestParseDatePhraseTestCases = map[string]time.Time{
+	"    1pm":                 time.Date(today.Year(), today.Month(), today.Day(), 13, 0, 0, 0, today.Location()),
+	"yesterday  at  3pm":      time.Date(today.Year(), today.Month(), today.Day()-1, 15, 0, 0, 0, today.Location()),
+	"last tuesday   at   3pm": time.Date(today.Year(), today.Month(), today.Day()-int(today.Weekday()-time.Tuesday)-7, 15, 0, 0, 0, today.Location()),
+	"next sunday   at   12pm": time.Date(today.Year(), today.Month(), today.Day()-int(today.Weekday()-time.Sunday)+7, 12, 0, 0, 0, today.Location()),
+	"this sunday   at   12am": time.Date(today.Year(), today.Month(), today.Day()-int(today.Weekday()-time.Sunday), 0, 0, 0, 0, today.Location()),
+}
+
 func TestParseTimeString(t *testing.T) {
 	t.Parallel()
 
 	var st, err = NewString2Time(today.Location())
 	assert.NoError(t, err)
 
-	result, err := st.parseTimeString(today, "  at  3pm")
-	assert.NoError(t, err)
-	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day(), 15, 0, 0, 0, today.Location()), result)
-
-	result, err = st.parseTimeString(today, "at 12pm")
-	assert.NoError(t, err)
-	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day(), 12, 0, 0, 0, today.Location()), result)
-
-	result, err = st.parseTimeString(today, "at 12am")
-	assert.NoError(t, err)
-	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, today.Location()), result)
+	for input, expected := range TestParseTimeStringTestCases {
+		result, err := st.parseTimeString(today, input)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, result)
+	}
 }
 
 func TestParseDatePhrase(t *testing.T) {
@@ -44,23 +46,9 @@ func TestParseDatePhrase(t *testing.T) {
 	var st, err = NewString2Time(today.Location())
 	assert.NoError(t, err)
 
-	result, err := st.parseDatePhrase("  1pm")
-	assert.NoError(t, err)
-	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day(), 13, 0, 0, 0, today.Location()), result)
-
-	result, err = st.parseDatePhrase("yesterday  at  3pm")
-	assert.NoError(t, err)
-	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day()-1, 15, 0, 0, 0, today.Location()), result)
-
-	result, err = st.parseDatePhrase("last tuesday   at   3pm")
-	assert.NoError(t, err)
-	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day()-int(today.Weekday()-time.Tuesday)-7, 15, 0, 0, 0, today.Location()), result)
-
-	result, err = st.parseDatePhrase("next sunday   at   12pm")
-	assert.NoError(t, err)
-	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day()-int(today.Weekday()-time.Sunday)+7, 12, 0, 0, 0, today.Location()), result)
-
-	result, err = st.parseDatePhrase("this sunday   at   12am")
-	assert.NoError(t, err)
-	assert.Equal(t, time.Date(today.Year(), today.Month(), today.Day()-int(today.Weekday()-time.Sunday), 0, 0, 0, 0, today.Location()), result)
+	for input, expected := range TestParseDatePhraseTestCases {
+		result, err := st.parseDatePhrase(input)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, result)
+	}
 }
