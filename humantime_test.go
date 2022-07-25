cbddd5f271bde3e0352bd1205852303d8d94a5cf
Kevin Mulvey <kmulvey@linux.com>
2022-07-25 15:26:43 -0600

    simplify test

--- humantime_test.go
+++ humantime_test.go
@@ -7,11 +7,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var now = time.Now()
+
+func init() {
+	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+}
+
+var TestParseTimeStringTestCases = map[string]time.Time{
+	"  at  3pm":  time.Date(now.Year(), now.Month(), now.Day(), 15, 0, 0, 0, now.Location()),
+	"at  12pm":   time.Date(now.Year(), now.Month(), now.Day(), 12, 0, 0, 0, now.Location()),
+	"  at  12am": time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()),
+}
+
 func TestParseTimeString(t *testing.T) {
 	t.Parallel()
 
-	var now = time.Now()
-	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	var st, err = NewString2Time(now.Location())
 	assert.NoError(t, err)
 
@@ -19,12 +29,10 @@ func TestParseTimeString(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day(), 15, 0, 0, 0, now.Location()), result)
 
-	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	result, err = st.parseTimeString(now, "at 12pm")
 	assert.NoError(t, err)
 	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day(), 12, 0, 0, 0, now.Location()), result)
 
-	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	result, err = st.parseTimeString(now, "at 12am")
 	assert.NoError(t, err)
 	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()), result)
@@ -33,8 +41,6 @@ func TestParseTimeString(t *testing.T) {
 func TestParseDatePhrase(t *testing.T) {
 	t.Parallel()
 
-	var now = time.Now()
-	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	var st, err = NewString2Time(now.Location())
 	assert.NoError(t, err)
 
@@ -42,22 +48,18 @@ func TestParseDatePhrase(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day(), 13, 0, 0, 0, now.Location()), result)
 
-	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	result, err = st.parseDatePhrase("yesterday  at  3pm")
 	assert.NoError(t, err)
 	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day()-1, 15, 0, 0, 0, now.Location()), result)
 
-	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	result, err = st.parseDatePhrase("last tuesday   at   3pm")
 	assert.NoError(t, err)
 	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day()-int(now.Weekday()-time.Tuesday)-7, 15, 0, 0, 0, now.Location()), result)
 
-	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	result, err = st.parseDatePhrase("next sunday   at   12pm")
 	assert.NoError(t, err)
 	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day()-int(now.Weekday()-time.Sunday)+7, 12, 0, 0, 0, now.Location()), result)
 
-	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	result, err = st.parseDatePhrase("this sunday   at   12am")
 	assert.NoError(t, err)
 	assert.Equal(t, time.Date(now.Year(), now.Month(), now.Day()-int(now.Weekday()-time.Sunday), 0, 0, 0, 0, now.Location()), result)
