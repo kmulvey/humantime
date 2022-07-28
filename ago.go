4fd4fd0fb97048fea786d044c1a32d07b0d42d0b
Kevin Mulvey <kmulvey@linux.com>
2022-07-28 16:20:19 -0600

    total rewrite of ago

--- ago.go
+++ ago.go
@@ -3,6 +3,7 @@ package humantime
 import (
 	"errors"
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -12,29 +13,121 @@ import (
 // and parses the remainder as time.Time, examples:
 // 3 hours ago
 // 8 days and three hours ago
+// 1 year 2 months 3 days 4 hours 5 minutes 6 seconds ago
 func (st *Humantime) Ago(input string) (*TimeRange, error) {
 	var tr = new(TimeRange)
+	var inputCopy = input
 
-	var inputArr = strings.Fields(input)
-	if len(inputArr) < 3 {
-		return tr, errors.New("input must have at least three fields")
+	// lint the input
+	if len(strings.Fields(input)) < 3 {
+		return nil, fmt.Errorf("input must have at least three fields: %s", input)
 	}
 	if !strings.HasSuffix(input, "ago") {
 		return nil, errors.New("input does not end with 'ago'")
 	}
 
-	var multiple, err = strconv.Atoi(inputArr[0])
+	// remove stop words
+	inputCopy = strings.ReplaceAll(input, "ago", "")
+	inputCopy = strings.ReplaceAll(inputCopy, "and", "")
+	inputCopy = strings.ReplaceAll(inputCopy, ",", "")
+	inputCopy = strings.TrimSpace(inputCopy)
+
+	// more linting
+	var inputArr = strings.Fields(inputCopy)
+	if len(inputArr)%2 != 0 {
+		return nil, fmt.Errorf("number of input fields must be even: %s", input)
+	}
+
+	var baseTime, nextString, err = parseLargeUnits(inputCopy, st.Location)
 	if err != nil {
-		return nil, fmt.Errorf("error parsing time: %s, err: %w", input, err)
+		return nil, fmt.Errorf("error parsing large units: %s, err: %w", input, err)
 	}
 
-	var unit, found = DurationWords[inputArr[1]]
-	if !found {
-		return nil, fmt.Errorf("could not parse %s", input)
+	duration, err := parseSmallUnits(nextString)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing small units: %s, err: %w", input, err)
 	}
 
-	tr.From = time.Now().Add(time.Duration(multiple*-1) * unit)
+	tr.From = *baseTime
+	tr.From = tr.From.Add(duration * -1)
 	tr.To = time.Now().In(st.Location)
 
 	return tr, nil
+}
+
+// parseLargeUnits takes the input string and parses out years, months and days.
+// It returns a Time pointer corresponding to now - [result of parsing].
+// It also returns the rest of the input string i.e. (full string - the part parsed).
+// The string should then be passed to parseSmallUnits().
+func parseLargeUnits(input string, loc *time.Location) (*time.Time, string, error) {
+	var year int
+	var month int
+	var day int
+	var tempNum int
+	var lastIndex int
+	var inputArr = strings.Fields(input)
+
+	var matched, err = regexp.MatchString(`year|month|day`, input)
+	if err != nil {
+		return nil, "", err
+	}
+	if matched {
+		for i, word := range strings.Fields(input) {
+			//fmt.Printf("i: %d, tempNum: %d, year: %d, month: %d, day: %d, word: %s \n", i, tempNum, year, month, day, word)
+			if i%2 == 0 {
+				tempNum, err = strconv.Atoi(word)
+				if err != nil {
+					return nil, "", fmt.Errorf("error parsing time: %s, err: %w", input, err)
+				}
+			} else {
+				if found := strings.Contains(word, "year"); found {
+					year = tempNum
+					lastIndex = i
+				} else if found := strings.Contains(word, "month"); found {
+					month = tempNum
+					lastIndex = i
+				} else if found := strings.Contains(word, "day"); found {
+					day = tempNum
+					lastIndex = i
+				}
+			}
+		}
+	}
+
+	var today = time.Now()
+	var t = time.Date(today.Year()-year, today.Month()-time.Month(month), today.Day()-day, today.Hour(), today.Minute(), today.Second(), 0, loc)
+	return &t, strings.Join(inputArr[lastIndex+1:], " "), nil
+}
+
+// parseSmallUnits takes the input string and parses out hours, minutes and seconds.
+// It does this by converting the string to the time.Duration string format: DhDmDs
+func parseSmallUnits(input string) (time.Duration, error) {
+
+	var matched, err = regexp.MatchString(`hour|minute|second`, input)
+	if err != nil {
+		return 0, err
+	}
+	if !matched {
+		return 0, nil
+	}
+
+	hr, err := regexp.Compile(`hour(s)?`)
+	if err != nil {
+		return 0, err
+	}
+	input = hr.ReplaceAllString(input, "h")
+
+	mr, err := regexp.Compile(`minute(s)?`)
+	if err != nil {
+		return 0, err
+	}
+	input = mr.ReplaceAllString(input, "m")
+
+	sr, err := regexp.Compile(`second(s)?`)
+	if err != nil {
+		return 0, err
+	}
+	input = sr.ReplaceAllString(input, "s")
+
+	return time.ParseDuration(strings.ReplaceAll(input, " ", ""))
 }
--- ago_test.go
+++ ago_test.go
@@ -7,19 +7,64 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var TestAgoTestCases = map[string]time.Time{
+	"3 days ago":   time.Date(today.Year(), today.Month(), today.Day()-3, today.Hour(), today.Minute(), today.Second(), 0, today.Location()),
+	"14 years ago": time.Date(today.Year()-14, today.Month(), today.Day(), today.Hour(), today.Minute(), today.Second(), 0, today.Location()),
+	"1 year 2 months 3 days 4 hours 5 minutes 6 seconds ago": time.Date(today.Year()-1, today.Month()-time.Month(2), today.Day()-3, today.Hour()-4, today.Minute()-5, today.Second()-6, 0, today.Location()),
+}
+
 func TestAgo(t *testing.T) {
 	t.Parallel()
 
 	var now = time.Now()
 	var st, err = NewString2Time(now.Location())
 	assert.NoError(t, err)
-	result, err := st.Ago("3 days ago")
-	assert.NoError(t, err)
-	assert.Equal(t, now.Add(time.Hour*24*-3).Round(time.Second), result.From.Round(time.Second))
-	assert.Equal(t, now.Round(time.Second), result.To.Round(time.Second))
 
-	result, err = st.Ago("14 years ago")
+	for input, expected := range TestAgoTestCases {
+		result, err := st.Ago(input)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, result.From)
+		assert.Equal(t, now.Round(time.Second), result.To.Round(time.Second))
+	}
+
+	result, err := st.Ago(" ago")
+	assert.Equal(t, "input must have at least three fields:  ago", err.Error())
+	assert.Nil(t, result)
+
+	result, err = st.Ago("2 years days ago")
+	assert.Equal(t, "number of input fields must be even: 2 years days ago", err.Error())
+	assert.Nil(t, result)
+
+	result, err = st.Ago("DD years ago")
+	assert.Equal(t, "error parsing large units: DD years ago, err: error parsing time: DD years, err: strconv.Atoi: parsing \"DD\": invalid syntax", err.Error())
+	assert.Nil(t, result)
+
+	result, err = st.Ago("DD seconds ago")
+	assert.Equal(t, "error parsing small units: DD seconds ago, err: time: invalid duration \"s\"", err.Error())
+	assert.Nil(t, result)
+
+	result, err = st.Ago("14 years before")
+	assert.Equal(t, "input does not end with 'ago'", err.Error())
+	assert.Nil(t, result)
+}
+
+func TestParseLargeUnits(t *testing.T) {
+	t.Parallel()
+
+	var today = time.Now()
+	var expected = time.Date(today.Year()-1, today.Month()-time.Month(2), today.Day()-3, today.Hour(), today.Minute(), today.Second(), 0, today.Location())
+	var result, nextString, err = parseLargeUnits("1 year 2 months 3 days 4 hours 5 minutes 6 seconds", today.Location())
 	assert.NoError(t, err)
-	assert.Equal(t, now.Add(time.Second*31536000*-14).Round(time.Second), result.From.Round(time.Second))
-	assert.Equal(t, now.Round(time.Second), result.To.Round(time.Second))
+	assert.Equal(t, &expected, result)
+	assert.Equal(t, "4 hours 5 minutes 6 seconds", nextString)
+}
+
+func TestParseSmallUnits(t *testing.T) {
+	t.Parallel()
+
+	var result, err = parseSmallUnits("4 hours 5 minutes 6 seconds")
+	assert.NoError(t, err)
+	d, err := time.ParseDuration("4h5m6s")
+	assert.NoError(t, err)
+	assert.Equal(t, d, result)
 }
